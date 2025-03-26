7597e20b768e051193d57a4d12e5b21aa8b00b41
sigmotoa <si.galvismotoa@gmail.com>
2025-03-26 18:52:24 -0500

    Action to retreive all pets from the file

--- operations.py
+++ operations.py
@@ -5,6 +5,8 @@ from models import *
 DATABASE_FILENAME = "pets.csv"
 column_fields = ["id", "name", "breed", "birth", "kind", "female"]
 
+
+#show all pets in file
 def read_all_pets():
     with open(DATABASE_FILENAME) as csvfile:
         reader = csv.DictReader(
