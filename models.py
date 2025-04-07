51aee01d7ccd467c3cb6d4679739f880f0acb3ac
sigmotoa <si.galvismotoa@gmail.com>
2025-04-07 16:01:06 -0500

    Added optional to the models

--- models.py
+++ models.py
@@ -4,10 +4,10 @@ from pydantic import BaseModel, Field
 
 class Pet(BaseModel):
     name:str = Field(..., min_length=3, max_length=20)
-    breed:str = Field(...,min_length=3,max_length=25)
-    birth:int = Field(...,gt=2000,lt=2025)
-    kind:str = Field(...,min_length=3,max_length=25)
-    female:bool=Field(...)
+    breed: Optional[str] = Field(None, min_length=3, max_length=25)  # Opcional
+    birth: Optional[int] = Field(None, gt=2000, lt=2025)  # Opcional
+    kind: Optional[str] = Field(None, min_length=3, max_length=25)  # Opcional
+    female: Optional[bool] = Field(None)  # Opcional
 
 class PetWithId(Pet):
     id:int
