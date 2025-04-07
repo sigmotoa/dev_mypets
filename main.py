bb7af588fa7eb3e2d766ae69c64fff6f2ded06fb
sigmotoa <si.galvismotoa@gmail.com>
2025-04-07 16:05:03 -0500

    Exposition of the first method to the DB.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi.params import Depends
+from sqlalchemy.testing.plugin.plugin_base import engines
 from starlette.responses import JSONResponse
+
+import models
 from models import *
 from operations import *
 from typing import List
+from contextlib import asynccontextmanager
+from database import Base
+from db_connection import AsyncSessionLocal, get_db_session, get_engine
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from db_operations import *
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    engine = get_engine()
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        yield
+    await engine.dispose()
+app = FastAPI(lifespan=lifespan)
 
 
 #pets:List[Pet]=[]
@@ -99,3 +117,15 @@ async def http_exception_handler(request,exc):
 @app.get("/error")
 async def raise_exception():
     raise HTTPException(status_code=400)
+
+
+@app.post("/dbpet", response_model=dict[str, int])
+async def add_pet(pet: models.Pet, db_session:Annotated[AsyncSession,Depends(get_db_session)]):
+    pet_id=await create_pet(db_session,
+                            pet.name,
+                            pet.breed,
+                            pet.birth,
+                            pet.kind,
+                            pet.female,)
+    return {"Nueva mascota":pet_id}
+
