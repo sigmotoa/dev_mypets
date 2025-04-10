f9828f0c9ebaee5071289b2953180a0d5e446775
sigmotoa <si.galvismotoa@gmail.com>
2025-04-09 21:42:49 -0500

    Directory with utilities. Terms contains a enums for the project

---
+++ utils/terms.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class Kind(Enum):
+    Dog = 1
+    Cat = 2
+    Bird = 3
+    Alligator = 4
+    Rabbit = 5
+    Hamster = 6
+
+
+class Genre(Enum):
+    Male="Male"
+    Female="Female"
