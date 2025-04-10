2eb878a2fbbc1f8cd0d4abdebdaa937c32ade3ba
sigmotoa <si.galvismotoa@gmail.com>
2025-04-09 23:02:41 -0500

    Connections and sessions

---
+++ sqlmodel_conn.py
@@ -0,0 +1,17 @@
+
+from sqlalchemy.ext.asyncio import AsyncSession,create_async_engine, AsyncEngine
+from sqlmodel import SQLModel
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL= "sqlite+aiosqlite:///.petsdb.db"
+
+engine : AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
+async_session =sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session():
+    async with async_session() as session:
+        yield session
