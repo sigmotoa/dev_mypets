d8b257cd4f4f96c9d03d59d6cf6ea7144f875488
sigmotoa <si.galvismotoa@gmail.com>
2025-04-11 21:50:48 -0500

    Fixing ID as optional, and adding the model config flag.

--- sqlmodel_db.py
+++ sqlmodel_db.py
@@ -1,29 +1,34 @@
+from tkinter.constants import CURRENT
 from typing import Optional
 
+from pydantic import ConfigDict
 from sqlalchemy import Boolean, table
 from sqlmodel import Field, SQLModel, create_engine, Session, select
 from datetime import datetime
 from utils.terms import *
 
+CURRENT_YEAR=datetime.now().year
 #Creation of the basic model of the pets
 class PetBase(SQLModel):
     name: str = Field(index=True, min_length=3, max_length=20)
     breed:str = Field(default=None, min_length=3, max_length=30)
     #Using new types to be pro 🤓
-    birth: datetime.year = Field(default=None)
+    birth: Optional[int] = Field(default=None, le=CURRENT_YEAR)
     kind: Kind = Field(default=None)
     genre: Genre = Field(default=None)
 
     #Adding a new fields to look cool
     image_path: Optional[str]=Field(default=None)
-    is_alive:Boolean = Field(default=True)
+    is_alive:bool = Field(default=True)
     created_at:datetime = Field(default_factory=datetime.now)
     updated_at:Optional[datetime] = Field(default=None)
 
 
-class Pet(PetBase, table=True):
+class PetSQL(PetBase, table=True):
     __tablename__ = "pets"
-    id: int = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 
