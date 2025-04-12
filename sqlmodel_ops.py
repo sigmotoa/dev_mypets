4f00352df4ee591981accc82244b9fb8711dde77
sigmotoa <si.galvismotoa@gmail.com>
2025-04-11 21:51:22 -0500

    Fixing the operations with an await calls.

--- sqlmodel_ops.py
+++ sqlmodel_ops.py
@@ -1,15 +1,15 @@
 from sqlmodel import Session, select
 from datetime import datetime
 from typing import List, Optional, Dict, Any
-from sqlmodel_db import Pet
+from sqlmodel_db import PetSQL
 
 #Creation of one pet in DB
-async def create_pet_sql(session: Session, pet:Pet):
-    db_pet = Pet.from_orm(pet)
+async def create_pet_sql(session: Session, pet:PetSQL):
+    db_pet = PetSQL.model_validate(pet, from_attributes=True)
     db_pet.created_at = datetime.now()
 
     session.add(db_pet)
-    session.commit()
-    session.refresh(db_pet)
+    await session.commit()
+    await session.refresh(db_pet)
 
     return db_pet
