c87c4a6d8479f74fed91be569ef3f939bc82d9e3
sigmotoa <si.galvismotoa@gmail.com>
2025-04-11 21:49:21 -0500

    Change of the terms in the enums.

--- utils/terms.py
+++ utils/terms.py
@@ -2,12 +2,12 @@ from enum import Enum
 
 
 class Kind(Enum):
-    Dog = 1
-    Cat = 2
-    Bird = 3
-    Alligator = 4
-    Rabbit = 5
-    Hamster = 6
+    Dog = "Dog"
+    Cat = "Cat"
+    Bird = "Bird"
+    Alligator = "Alligator"
+    Rabbit = "Rabbit"
+    Hamster = "Hamster"
 
 
 class Genre(Enum):
