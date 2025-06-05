3bf8329d8e72012d120d34db7af6c7f5ff843ae1
sigmotoa <si.galvismotoa@gmail.com>
2025-06-05 12:28:19 -0500

    Fixing to render the correct template

--- routers/pets/web.py
+++ routers/pets/web.py
@@ -18,7 +18,7 @@ templates = Jinja2Templates(directory="templates")
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("pets/home.html", {"request": request})
 
 
 #All pets
