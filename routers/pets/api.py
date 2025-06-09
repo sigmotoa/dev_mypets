8abf1c615701d38cf385dde31caaf3d8df68c8ad
sigmotoa <si.galvismotoa@gmail.com>
2025-06-08 21:02:18 -0500

    Removing the endpoints to migrate to the API router

--- routers/pets/api.py
+++ routers/pets/api.py
@@ -49,3 +49,42 @@ async def create_pet_img(
     #await session.commit()
     #await session.refresh(pet)
     return pet
+
+
+#Get one pet
+@router.get("/pets/{pet_id}", response_model=PetSQL, tags=["SQLMODEL"])
+async def read_pet_img(pet_id:int, session:Session = Depends(get_session)):
+    pet = await crud.get_pet(session=session, pet_id=pet_id)
+    if pet is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Mascota con img no encontrada"
+        )
+    return pet
+
+
+#Get all pets
+
+@router.get("/pets", response_model=list[PetSQL], tags=["SQLMODEL"])
+async def read_pets_img(session:Session = Depends(get_session)):
+    pets = await crud.get_all_pets(session=session)
+    return pets
+
+
+
+#Update one pet
+@router.patch("/pets/{pet_id}", response_model=PetSQL, tags=["SQLMODEL"])
+async def update_pet_img(pet_id:int, pet_update:PetSQL,session:Session=Depends(get_session)):
+    pet = await crud.update_pet(session, pet_id, pet_update.dict(exclude_unset=True))
+    if pet is None:
+        raise HTTPException(status_code=404, detail="Mascota no encontrada para actualizar")
+    return pet
+
+
+#Deactive a pet
+@router.patch("/pets/{pet_id}", response_model=PetSQL, tags=["SQLMODEL"])
+async def deactive_pet_img(pet_id:int, sesion:Session=Depends(get_session)):
+    pet = await crud.mark_pet_inactive(sesion, pet_id)
+    if pet is None:
+        raise HTTPException(status_code=404, detail="Mascota no encontrada para desactivar")
+    return pet
